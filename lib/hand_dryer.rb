cf215ef0cc1b70799c81888c0d5990d77254efba
Radek Zika <radek.zika@gmail.com>
2016-04-06 23:56:21 -0500

    gem works now

--- lib/hand_dryer.rb
+++ lib/hand_dryer.rb
@@ -1,5 +1,11 @@
 require "hand_dryer/version"
 
 module HandDryer
+  class Dryer
+    def dry
+      puts "Hands wet?"
+      puts "Drying ...!"
+    end
+  end
   # Your code goes here...
 end
--- lib/hand_dryer/version.rb
+++ lib/hand_dryer/version.rb
@@ -1,3 +1,3 @@
 module HandDryer
-  VERSION = "0.1.0"
+  VERSION = "0.1.0".freeze
 end
